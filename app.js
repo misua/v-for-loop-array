07cefdce2c23a0b5d77cfd2e191858c7a92b90e5
Charles <charlespogzz@googlemail.com>
2021-04-28 23:49:28 +0800

    v-for-loop
    adding array contents using v-for-loop

---
+++ app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+   data() {
+       return {
+           showBooks: true,
+           books: [
+               { title: 'name of the wind', author: 'patrick rothfuss' },
+               { title: 'the way of the kings', author: 'brandon sanderson' },
+               { title: 'the final empire', author: 'brandon sanderson' }
+           ]
+       }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+         },
+       
+       
+       
+         //  handleEvent(e, data) {
+        //      console.log(e, e.type)
+        //      if (data){
+        //          console.log(data)
+        //      }
+        //  },
+        //  handleMousemove(e) {
+        //     this.x = e.offsetX
+        //     this.y = e.offsetY
+
+        //}
+    }   
+})
+
+
+app.mount('#app') 
